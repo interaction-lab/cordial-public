25c288bd0c8169dc76940905c72af54204895928
QT138-ILab <interactionlab@gmail.com>
2020-01-13 17:31:07 -0800

    dos2unix files and behaviors updated

--- cordial_behavior/scripts/behaviors.py
+++ cordial_behavior/scripts/behaviors.py
@@ -9,9 +9,9 @@ from std_msgs.msg import String
 from cordial_behavior.msg import *
 from cordial_face.msg import FaceRequest
 from threading import Timer
-"""
+
 data = "[{'start': 0.006, 'type': 'viseme', 'id': 'DENTAL_ALVEOLAR'}, {'start': 0.075, 'type': 'viseme', 'id': 'POSTALVEOLAR'}, {'start': 0.136, 'type': 'viseme', 'id': 'OPEN_FRONT_VOWEL'}, {'start': 0.264, 'type': 'viseme', 'id': 'DENTAL_ALVEOLAR'}, {'start': 0.332, 'type': 'viseme', 'id': 'CLOSE_BACK_VOWEL'}, {'start': 0.383, 'type': 'viseme', 'id': 'VELAR_GLOTTAL'}, {'start': 0.463, 'type': 'viseme', 'id': 'CLOSE_FRONT_VOWEL'}, {'start': 0.535, 'type': 'viseme', 'id': 'DENTAL_ALVEOLAR'}, {'start': 0.571, 'type': 'viseme', 'id': 'MID_CENTRAL_VOWEL'}, {'start': 0.581, 'args': [], 'type': 'action', 'id': 'behav1'}, {'start': 0.611, 'type': 'viseme', 'id': 'BILABIAL'}, {'start': 0.689, 'type': 'viseme', 'id': 'CLOSE_FRONT_VOWEL'}, {'start': 0.73, 'type': 'viseme', 'id': 'VELAR_GLOTTAL'}, {'start': 0.829, 'type': 'viseme', 'id': 'OPEN_FRONT_VOWEL'}, {'start': 0.951, 'type': 'viseme', 'id': 'LABIODENTAL'}, {'start': 1.009, 'type': 'viseme', 'id': 'CLOSE_FRONT_VOWEL'}, {'start': 1.076, 'type': 'viseme', 'id': 'OPEN_FRONT_VOWEL'}, {'start': 1.174, 'type': 'viseme', 'id': 'CLOSE_FRONT_VOWEL'}, {'start': 1.254, 'type': 'viseme', 'id': 'DENTAL_ALVEOLAR'}, {'start': 1.307, 'type': 'viseme', 'id': 'INTERDENTAL'}, {'start': 1.337, 'type': 'viseme', 'id': 'MID_CENTRAL_VOWEL'}, {'start': 1.368, 'type': 'viseme', 'id': 'DENTAL_ALVEOLAR'}, {'start': 1.3780000000000001, 'args': [], 'type': 'action', 'id': 'behav2'}, {'start': 1.51, 'type': 'viseme', 'id': 'OPEN_FRONT_VOWEL'}, {'start': 1.617, 'type': 'viseme', 'id': 'BILABIAL'}, {'start': 1.699, 'type': 'viseme', 'id': 'DENTAL_ALVEOLAR'}, {'start': 1.804, 'type': 'viseme', 'id': 'OPEN_FRONT_VOWEL'}, {'start': 1.857, 'type': 'viseme', 'id': 'DENTAL_ALVEOLAR'}, {'start': 1.903, 'type': 'viseme', 'id': 'DENTAL_ALVEOLAR'}, {'start': 1.977, 'type': 'viseme', 'id': 'DENTAL_ALVEOLAR'}, {'start': 2.107, 'type': 'viseme', 'id': 'DENTAL_ALVEOLAR'}, {'start': 2.281, 'type': 'viseme', 'id': 'IDLE'}]"
-"""
+
 
 class BehaviorManager():
 
@@ -20,63 +20,35 @@ class BehaviorManager():
 		self.nuc_topic = nuc
 		rospy.init_node("behavior_node", anonymous=True)
 		rospy.Subscriber(self.pi_topic+'/behavior', String, self.handle_behavior)
-		self._behavior_client = actionlib.SimpleActionClient('/DB1/Behavior',BehaviorAction)
 		self.face_publisher = rospy.Publisher('/DB1/face', FaceRequest, queue_size=10)
+		self.gesture_publisher = rospy.Publisher(self.nuc_topic+'/gestures', String, queue_size=10)
+		self.handle_behavior(data)
 		rospy.spin()
 	
 
 	def handle_behavior(self, data):
 		print("Handle behaviors")
-		behav = literal_eval(data.data)
+		#behav = literal_eval(data.data)
+		behav = literal_eval(data)
 		visemes = ["BILABIAL","LABIODENTAL","INTERDENTAL","DENTAL_ALVEOLAR","POSTALVEOLAR","VELAR_GLOTTAL","CLOSE_FRONT_VOWEL","OPEN_FRONT_VOWEL","MID_CENTRAL_VOWEL","OPEN_BACK_VOWEL","CLOSE_BACK_VOWEL", 'IDLE']
 		gesture_behaviors = filter(lambda b: b["id"] not in visemes, behav)
 		viseme_behaviors = filter(lambda b: b["id"] in visemes, behav)
 		self.handle_visemes(viseme_behaviors)
+		self.handle_gestures(gesture_behaviors)
 
 
 	def handle_gestures(self, gesture_behaviors):
 		#Handle Gesures
-		"""
+		print("Handle gestures")
 		ordered_behaviors = sorted(gesture_behaviors, key=lambda behavior: behavior["start"])
 		start_time = rospy.Time.now()
-        for a in ordered_behaviors:
-            rospy.loginfo("Playing behavior: " + str(a))
-            while rospy.Time.now()-start_time < rospy.Duration.from_sec(a["start"])+timing_adjust and not self._server.is_preempt_requested():
-                pass
-            if self._server.is_preempt_requested():
-                preempted = True
-                break
-
-
-            if_conflict = BehaviorGoal.DROP
-            if "interrupt" in a.keys():
-                if_conflict = BehaviorGoal.OVERRIDE
-
-            if "args" in a.keys():
-                args = a["args"]
-            else:
-                args = []
-
-            if "hold" in a.keys():
-                hold = bool(a["hold"])
-            else:
-                hold = True # default is yes, hold pose until told otherwise
-
-            if "move_time" in a.keys():
-                end_move = rospy.Time.now()+rospy.Duration.from_sec(a["move_time"])
-            else:
-                end_move = rospy.Time()
-
-            if "end" in a.keys():
-                end_hold = rospy.Duration.from_sec(a["end"])+start_time
-            else:
-                end_hold = rospy.Time()
-
-            goal = BehaviorGoal(behavior=a["id"], end_hold=end_hold, end_move=end_move, return_to_prior=not hold, if_conflict = if_conflict, args = args, wait_and_block = False) #old version
-
-            self._behavior_client.send_goal(goal) #old version
-            return
-            """
+		for behav in ordered_behaviors:
+		    rospy.loginfo("Playing behavior: " + str(behav))    
+		    while rospy.Time.now()-start_time < rospy.Duration.from_sec(behav["start"]):
+		        pass
+		    rospy.loginfo("Play " + str(behav["id"]) + " at time:" + str(behav["start"]))
+		    self.gesture_publisher.publish(behav["id"])
+            
 		
 		
 
@@ -93,9 +65,7 @@ class BehaviorManager():
 		viseme_speed = 10
 		#timing_adjust = rospy.Duration.from_sec(0)
 		viseme_req = FaceRequest(visemes=viseme_ids, viseme_ms=viseme_speed, times=viseme_times)
-		#print(viseme_req)
 		#viseme_delay_time = timing_adjust.to_sec()
-		#print(viseme_delay_time)
 		def send_visemes():
 			print("Send viseme")
 			self.face_publisher.publish(viseme_req)
--- cordial_face/scripts/setup_http_servers.sh
+++ cordial_face/scripts/setup_http_servers.sh
@@ -3,7 +3,7 @@
 killall http-server
 
 source /opt/ros/kinetic/setup.bash
-source /home/qtrobot/test_ws/devel/setup.bash
+source /home/qtrobot/catkin_ws/devel/setup.bash
 
 roscd cordial_face/web && http-server -p $1 &
 roscd cordial_tablet/web && http-server -p $2 &
